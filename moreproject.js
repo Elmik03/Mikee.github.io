bf8dc7f0e4ce6ad9f9685549f06a46b1937e1818
Michael <146709617+Elmik03@users.noreply.github.com>
2024-09-17 10:52:58 +0100

    adding more-projects

---
+++ moreproject.js
@@ -0,0 +1,24 @@
+
+
+links = [
+ 
+]
+
+//alert (links [2])
+
+/*
+let t = document.createElement("ul");
+     //let l = links.length();
+     for (let link of links3)//i = 0, i <= l, i++)
+      {let li = document.createElement("li");
+     let a = document.createElement("a");
+     let br = document.createElement("br");
+         a.href = link;
+         a.textContent = link.toUpperCase();
+         li.append(a);
+         t.append(li);
+         }*/
+//console.log(t.innerHTML);
+
+    // document.body.textContent = t;
+     //console.log(document.body.innerHTML);
